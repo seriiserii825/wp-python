7d0b784658b20a1dc0d86695b74cf4d04bbd11d2
serii <seriiburduja@gmail.com>
2024-04-08 13:49:47 +0300

    feat: plugins

--- utils/plugins.py
+++ utils/plugins.py
@@ -4,6 +4,16 @@ from termcolor import colored
 from libs.select import selectMultiple
 
 def pluginsFunc():
+    base_plugins = [
+            {"advanced-custom-fields-pro": "advanced-custom-fields-pro-6_0_6.zip"},
+            {"all-in-one-wp-migration": "all-in-one-wp-migration-7-79.zip"},
+            {"classic-editor": False},
+            {"tinymce-advanced": False},
+            {"stops-core-theme-and-plugin-updates": False},
+            {"safe-svg": False},
+            {"wps-hide-login": False},
+            {"wps-limit-login": False},
+            ]
     plugins = [
             {"advanced-custom-fields-pro_6_0_6": "advanced-custom-fields-pro-6_0_6.zip"},
             {"advanced-custom-fields-pro-6_2_6_1": "advanced-custom-fields-pro-6_2_6_1.zip"},
@@ -17,6 +27,7 @@ def pluginsFunc():
             {"wp-mail-smtp": False},
             {"cookie-notice": False},
             {"wps-hide-login": False},
+            {"wps-limit-login": False},
             {"seo-by-rank-math":"seo-by-rank-math.zip"},
             {"wp-pagenavi": False},
             {"error-log-monitor": False},
@@ -39,18 +50,43 @@ def pluginsFunc():
 
     def listInstalledPlugins():
         os.system("wp plugin list")
-        menu()
 
     def getInstalledPlugins():
         installedPlugins = []
+        current_dir = os.getcwd()
         os.chdir("../../plugins")
         for plugin in os.listdir():
             path = os.path.join(".", plugin)
             if os.path.isdir(path):
                 installedPlugins.append(plugin)
+        os.chdir(current_dir)
         return installedPlugins
 
     def installBasePlugins(plugins):
+        installed_plugins = getInstalledPlugins()
+        print(installed_plugins)
+        installed_plugins.sort()
+        not_installed_plugins = []
+        for plugin in plugins:
+            for key, value in plugin.items():
+                if key not in installed_plugins:
+                    not_installed_plugins.append(plugin)
+
+        if len(not_installed_plugins) == 0:
+            print(colored("All plugins are installed!", "red"))
+            exit()
+
+        for plugin in not_installed_plugins:
+            for key, value in plugin.items():
+                print(f"Key: {key}, Value: {value}")
+                if key in plugin:
+                    print(f"Plugin {key} is already installed!")
+                    if value == False:
+                        os.system("wp plugin install " + key + " --activate")
+                    else:
+                        os.system("wp plugin install ~/Documents/plugins-wp/" + value + " --activate")
+
+    def installPlugins(plugins):
         installed_plugins = getInstalledPlugins()
         installed_plugins.sort()
         not_installed_plugins = []
@@ -77,7 +113,6 @@ def pluginsFunc():
                         os.system("wp plugin install " + key + " --activate")
                     else:
                         os.system("wp plugin install ~/Documents/plugins-wp/" + value + " --activate")
-        exit()
 
     def uninstallPlugins(plugins):
         installed_plugins = getInstalledPlugins()
@@ -93,22 +128,28 @@ def pluginsFunc():
                 if key in selected_plugins:
                     os.system("wp plugin deactivate " + key)
                     os.system("wp plugin uninstall " + key)
-        exit()
 
     def menu():
         print(colored("1. List", "yellow"))
-        print(colored("2. Install", "green"))
-        print(colored("3. Uninstall", "blue"))
-        print(colored("4. Exit", "red"))
+        print(colored("2. Install Base Plugins", "green"))
+        print(colored("3. Install Plugins", "green"))
+        print(colored("4. Uninstall", "blue"))
+        print(colored("5. Exit", "red"))
 
         action = input("Choose action: ")
         if action == "1":
             listInstalledPlugins()
+            menu()
         elif action == "2":
-            installBasePlugins(plugins)
+            installBasePlugins(base_plugins)
+            menu()
         elif action == "3":
-            uninstallPlugins(plugins)
+            installPlugins(plugins)
+            menu()
         elif action == "4":
+            uninstallPlugins(plugins)
+            menu()
+        elif action == "5":
             exit()
         else:
             print("Wrong action!")
