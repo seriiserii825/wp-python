ea84f064b6cdedd23956ef271439f4a88b305d74
serii <seriiburduja@gmail.com>
2024-04-10 12:33:04 +0300

    feat: acf-adon plugins added

--- utils/plugins.py
+++ utils/plugins.py
@@ -19,6 +19,7 @@ def pluginsFunc():
             {"advanced-custom-fields-pro-6_2_6_1": "advanced-custom-fields-pro-6_2_6_1.zip"},
             {"all-in-one-wp-migration": "all-in-one-wp-migration-7-79.zip"},
             {"admin-columns-pro": "admin-columns-pro-5.4.3.zip"},
+            {"ac-addon-acf-2.6.4": "ac-addon-acf-2.6.4.zip"},
             {"classic-editor": False},
             {"tinymce-advanced": False},
             {"stops-core-theme-and-plugin-updates": False},
