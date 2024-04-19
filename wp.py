a36fc533ed6394bb539fd56145b9b8f5052741c4
serii <seriiburduja@gmail.com>
2024-04-19 12:42:58 +0300

    feat: delete convert fonts and create project

--- wp.py
+++ wp.py
@@ -4,8 +4,6 @@ from termcolor import colored
 from acf.acf import acfFunc
 from init import init
 from utils.aiwm import aiwmFunc
-from utils.convert_fonts import convertFontsFunc
-from utils.create_project import createProject
 from utils.pages import wpPages
 from utils.plugins import pluginsFunc
 from utils.themes import wpThemesFunc
@@ -17,12 +15,10 @@ print(colored("2) Files", "blue"))
 print(colored("3) Acf", "yellow"))
 print(colored("4) Backups", "green"))
 print(colored("5) Plugins", "blue"))
-print(colored("6) Create Project", "blue"))
-print(colored("7) Convert Fonts", "yellow"))
-print(colored("8) Themes", "green"))
-print(colored("9) Pages", "blue"))
-print(colored("10) Images", "blue"))
-print(colored("11) Exit", "red"))
+print(colored("6) Themes", "green"))
+print(colored("7) Pages", "blue"))
+print(colored("8) Images", "blue"))
+print(colored("9) Exit", "red"))
 
 choice = input("Enter your choice: ")
 if choice == '1':
@@ -36,14 +32,10 @@ elif choice == '4':
 elif choice == '5':
     pluginsFunc()
 elif choice == '6':
-    createProject()
-elif choice == '7':
-    convertFontsFunc()
-elif choice == '8':
     wpThemesFunc()
-elif choice == '9':
+elif choice == '7':
     wpPages()
-elif choice == '10':
+elif choice == '8':
     wpImages()
 else:
     exit()
