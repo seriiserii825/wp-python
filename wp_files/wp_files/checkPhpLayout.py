ef2f776ff64c77dfe0d356294a3118742d937366
serii <seriiburduja@gmail.com>
2024-07-16 16:00:44 +0300

    feat: update layout php

--- wp_files/wp_files/checkPhpLayout.py
+++ wp_files/wp_files/checkPhpLayout.py
@@ -6,6 +6,10 @@ def checkPhpLayout(layout_path):
     if not os.path.exists(layout_path):
         # create this file
         print(colored("Creating php layout file...", "green"))
+        dir_path=os.path.dirname(layout_path)
+        os.system(f"mkdir -p {dir_path}")
+        os_command = f"touch {layout_path}"
+        os.system(os_command)
         with open(layout_path, 'w') as f:
             layout_code = """
             <?php
