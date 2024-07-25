48473c1b861b0fc5883a81857ac680e8b39dbd64
serii <seriiburduja@gmail.com>
2024-07-25 18:33:04 +0300

    feat: chrome browser dont work

---
+++ backups/backups.py
@@ -0,0 +1,151 @@
+import os
+import glob
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+from urllib.parse import urlparse
+from pick import pick
+from termcolor import colored
+from libs.listDir import listDir
+from wp_files.wp_files.createOrChooseDirectory import createOrChooseDirectory
+from libs.orderFiles import orderFiles
+
+def listBackup():
+    os.system("wp ai1wm list-backups")
+
+def restoreBackupInChrome():
+    # options = webdriver.ChromeOptions() 
+    # options.add_argument("user-data-dir=/home/serii/.config/google-chrome/Default") #Path to your chrome profile
+    # options.add_argument('--profile-directory=/home/serii/.config/google-chrome/Default')
+    # service = Service(executable_path='/usr/bin/chromedriver')
+    # driver = webdriver.Chrome(service=service, options=options)
+    # driver.get("https://www.google.com")
+
+# Specify the debugging address for the already opened Chrome browser
+
+# Set up ChromeOptions and connect to the existing browser
+    options = Options()
+    options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
+    service = Service(executable_path='/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get('https://www.google.com')
+    # element = driver.find_element_by_id('element-id')
+    # element.click()
+
+    time.sleep(300)
+    driver.quit()
+
+    # driver = webdriver.Chrome(service=service)
+    # driver.get("https://www.google.com")
+#     timeout = 1
+#     input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+# # time.sleep(timeout)
+#     input_element.clear()
+#     input_element.send_keys(search_text)
+# # time.sleep(timeout)
+#     input_element.send_keys(Keys.RETURN)
+#     link = driver.find_element(By.PARTIAL_LINK_TEXT, search_text)
+# # time.sleep(timeout)
+#     link.click()
+
+def createAndCopyToMnt():
+    directory_exists = os.path.isdir('/mnt/Projects')
+    if directory_exists:
+        path_to_dir = '/mnt/Projects'
+        listDir(path_to_dir)
+        selected_dir = createOrChooseDirectory(path_to_dir)
+        path_to_selected_dir = path_to_dir + "/" + selected_dir
+        listDir(path_to_selected_dir)
+        selected_project = createOrChooseDirectory(path_to_selected_dir)
+        path_to_selected_dir = path_to_selected_dir + "/" + selected_project
+        sorted_files = orderFiles(path_to_selected_dir)
+        makeBackup(path_to_selected_dir)
+        print(colored(f"Backup file copied to {path_to_selected_dir}", "blue"))
+        for file in sorted_files:
+            print(file)
+    else:
+        exit(colored("Directory /mnt/Projects not exists!", "red"))
+def makeBackup(path_to_project=''):
+    listBackup()
+    os.system("wp ai1wm backup")
+    list_of_files = glob.glob('../../ai1wm-backups/*.wpress')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    if not list_of_files:
+        backup_files = os.listdir(".")
+        for file in backup_files:
+            if file.endswith('.wpress'):
+                os.system(f"cp {file} ~/Downloads")
+                if path_to_project != "":
+                    os.system(f"cp {file} {path_to_project}")
+        listBackup()
+    else:
+        latest_file = max(list_of_files, key=os.path.getctime)
+        os.system(f"cp {latest_file} ~/Downloads")
+        if path_to_project != "":
+            os.system(f"cp {latest_file} {path_to_project}")
+        listBackup()
+    print(colored(f"Backup file: {latest_file}", "blue"))
+def restoreBackup():
+    listBackup()
+    os.chdir("../../ai1wm-backups")
+    backup_files = os.listdir()
+    title = 'Select plugins'
+    backups_array = []
+    selected_plugins = []
+    for file in backup_files:
+        if file.endswith('.wpress'):
+            backups_array.append(file)
+    selected = pick(backups_array, title, multiselect=False, min_selection_count=1)
+    print(selected)
+    for item in selected:
+        selected_plugins.append(item)
+    os.system(f"wp ai1wm restore {selected_plugins[0]}")
+def restoreFromDownloads():
+    downloads_dir = os.path.expanduser("~/Downloads")
+    backup_files = orderFiles(downloads_dir)
+    title = 'Select files'
+    backups_array = []
+    selected_plugins = []
+    for file in backup_files:
+        if file.endswith('.wpress'):
+            backups_array.append(file)
+    selected = pick(backups_array, title, multiselect=False, min_selection_count=1)
+    for item in selected:
+        selected_plugins.append(item)
+    os.system(f"cp ~/Downloads/{selected_plugins[0]} ../../ai1wm-backups")
+    print(f"Selected file: {selected_plugins[0]}")
+    os.system(f"wp ai1wm restore {selected_plugins[0]}")
+def downloadBackup():
+    domain_url = input("Enter domain url: ")
+    if domain_url == "":
+        exit(colored("Domain url is empty!", "red"))
+    domain = urlparse(domain_url).netloc
+    domain = 'https://' + domain
+    backup_file_name = input("Enter backup file name: ")
+    if backup_file_name == "":
+        exit(colored("Backup file name is empty!", "red"))
+    os.chdir("../../ai1wm-backups")
+    os.system(f"wget {domain}/wp-content/ai1wm-backups/{backup_file_name}")
+    os.system(f"wp ai1wm restore {backup_file_name}")
+    os.system("wp rewrite flush")
+def deleteBackup():
+    os.chdir("../../ai1wm-backups")
+    backup_files = os.listdir()
+    #sort by ctime in reverse order
+    backup_files.sort(key=lambda x: os.path.getctime(x), reverse=True)
+    title = 'Select files'
+    backups_array = []
+    selected_plugins = []
+    for file in backup_files:
+        if file.endswith('.wpress'):
+            backups_array.append(file)
+    selected = pick(backups_array, title, multiselect=True, min_selection_count=1)
+    for item in selected:
+        selected_plugins.append(item[0])
+    for file in selected_plugins:
+        os.system(f"rm -f {file}")
--- utils/aiwm.py
+++ utils/aiwm.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 import os
 from termcolor import colored
-from urllib.parse import urlparse
-from pick import pick
-from pyfzf.pyfzf import FzfPrompt
-import glob
 
-from libs.listDir import listDir
-from libs.listFiles import listFiles
-from libs.orderFiles import orderFiles
-from libs.select import selectOne
-from wp_files.wp_files.createOrChooseDirectory import createOrChooseDirectory
-fzf = FzfPrompt()
+from backups.backups import createAndCopyToMnt, deleteBackup, downloadBackup, listBackup, makeBackup, restoreBackup, restoreBackupInChrome, restoreFromDownloads
 
 is_installed_plugins = os.path.exists("../../plugins/all-in-one-wp-migration");
 if not is_installed_plugins:
@@ -22,114 +13,6 @@ if not is_installed_unlimited:
     os.system("wp plugin install ~/Documents/plugins-wp/unlimited/all-in-one-wp-migration-unlimited-extension-2.51.zip --activate")
 
 def aiwmFunc():
-    def listBackup():
-        os.system("wp ai1wm list-backups")
-
-    def createAndCopyToMnt():
-        directory_exists = os.path.isdir('/mnt/Projects')
-        if directory_exists:
-            path_to_dir = '/mnt/Projects'
-            listDir(path_to_dir)
-            selected_dir = createOrChooseDirectory(path_to_dir)
-            path_to_selected_dir = path_to_dir + "/" + selected_dir
-            listDir(path_to_selected_dir)
-            selected_project = createOrChooseDirectory(path_to_selected_dir)
-            path_to_selected_dir = path_to_selected_dir + "/" + selected_project
-            sorted_files = orderFiles(path_to_selected_dir)
-            makeBackup(path_to_selected_dir)
-            print(colored(f"Backup file copied to {path_to_selected_dir}", "blue"))
-            for file in sorted_files:
-                print(file)
-        else:
-            exit(colored("Directory /mnt/Projects not exists!", "red"))
-
-    def makeBackup(path_to_project=''):
-        listBackup()
-        os.system("wp ai1wm backup")
-        list_of_files = glob.glob('../../ai1wm-backups/*.wpress')
-        latest_file = max(list_of_files, key=os.path.getctime)
-        if not list_of_files:
-            backup_files = os.listdir(".")
-            for file in backup_files:
-                if file.endswith('.wpress'):
-                    os.system(f"cp {file} ~/Downloads")
-                    if path_to_project != "":
-                        os.system(f"cp {file} {path_to_project}")
-            listBackup()
-        else:
-            latest_file = max(list_of_files, key=os.path.getctime)
-            os.system(f"cp {latest_file} ~/Downloads")
-            if path_to_project != "":
-                os.system(f"cp {latest_file} {path_to_project}")
-            listBackup()
-        print(colored(f"Backup file: {latest_file}", "blue"))
-    def restoreBackup():
-        listBackup()
-        os.chdir("../../ai1wm-backups")
-        backup_files = os.listdir()
-
-        title = 'Select plugins'
-        backups_array = []
-        selected_plugins = []
-        for file in backup_files:
-            if file.endswith('.wpress'):
-                backups_array.append(file)
-        selected = pick(backups_array, title, multiselect=False, min_selection_count=1)
-        print(selected)
-        for item in selected:
-            selected_plugins.append(item)
-
-        os.system(f"wp ai1wm restore {selected_plugins[0]}")
-
-    def restoreFromDownloads():
-        downloads_dir = os.path.expanduser("~/Downloads")
-        backup_files = orderFiles(downloads_dir)
-        title = 'Select files'
-        backups_array = []
-        selected_plugins = []
-        for file in backup_files:
-            if file.endswith('.wpress'):
-                backups_array.append(file)
-        selected = pick(backups_array, title, multiselect=False, min_selection_count=1)
-        for item in selected:
-            selected_plugins.append(item)
-        os.system(f"cp ~/Downloads/{selected_plugins[0]} ../../ai1wm-backups")
-        print(f"Selected file: {selected_plugins[0]}")
-        os.system(f"wp ai1wm restore {selected_plugins[0]}")
-
-    def downloadBackup():
-        domain_url = input("Enter domain url: ")
-        if domain_url == "":
-            exit(colored("Domain url is empty!", "red"))
-        domain = urlparse(domain_url).netloc
-        domain = 'https://' + domain
-
-        backup_file_name = input("Enter backup file name: ")
-        if backup_file_name == "":
-            exit(colored("Backup file name is empty!", "red"))
-
-        os.chdir("../../ai1wm-backups")
-        os.system(f"wget {domain}/wp-content/ai1wm-backups/{backup_file_name}")
-        os.system(f"wp ai1wm restore {backup_file_name}")
-        os.system("wp rewrite flush")
-
-    def deleteBackup():
-        os.chdir("../../ai1wm-backups")
-        backup_files = os.listdir()
-        #sort by ctime in reverse order
-        backup_files.sort(key=lambda x: os.path.getctime(x), reverse=True)
-
-        title = 'Select files'
-        backups_array = []
-        selected_plugins = []
-        for file in backup_files:
-            if file.endswith('.wpress'):
-                backups_array.append(file)
-        selected = pick(backups_array, title, multiselect=True, min_selection_count=1)
-        for item in selected:
-            selected_plugins.append(item[0])
-        for file in selected_plugins:
-            os.system(f"rm -f {file}")
 
     def menu():
         print(colored("1) List", "green"))
@@ -137,9 +20,10 @@ def aiwmFunc():
         print(colored("2.1) Create and copy to mnt", "yellow"))
         print(colored("3) Restore", "blue"))
         print(colored("4) Restore from Downloads", "blue"))
-        print(colored("5) Download Backup", "yellow"))
-        print(colored("6) Delete backups", "red"))
-        print(colored("7) Exit", "red"))
+        print(colored("5) Restore in Browser", "blue"))
+        print(colored("6) Download Backup", "yellow"))
+        print(colored("7) Delete backups", "red"))
+        print(colored("8) Exit", "red"))
 
         choice = input("Make your choice:")
         if choice == "1":
@@ -158,9 +42,12 @@ def aiwmFunc():
             restoreFromDownloads()
             menu()
         elif choice == "5":
-            downloadBackup()
+            restoreBackupInChrome()
             menu()
         elif choice == "6":
+            downloadBackup()
+            menu()
+        elif choice == "7":
             deleteBackup()
             menu()
         else:
