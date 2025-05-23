4c0206ed0b03d9306bc4d6ca40fae29e77bda2fc
serii burduja <seriiburduja@gmail.com>
2025-05-23 13:40:19 +0300

    feat: post type and pages delete all

--- utils/pages.py
+++ utils/pages.py
@@ -3,10 +3,12 @@ import os
 from simple_term_menu import subprocess
 from termcolor import colored
 
+
 def createOne(post_type="page"):
     title = input("Enter the title: ")
     command = f"wp post create --post_type={post_type} --post_status=publish --post_title='{title}'"
     os.system(command)
+
 
 def createMultiple(post_type="page"):
     titles = input("Enter the titles separated by commas: ")
@@ -17,32 +19,52 @@ def createMultiple(post_type="page"):
         command = f"wp post create --post_type={post_type} --post_status=publish --post_title='{title}'"
         os.system(command)
 
+
 def deleteOne(post_type="page"):
     command = f"wp post list --post_type={post_type}"
     os.system(command)
     ids = input("Enter the id of the page you want to delete: ")
     os.system(f"wp post delete {ids} --force")
 
+
 def deleteMultiple(post_type="page"):
-    command = f"wp post list --post_type={post_type}"
-    os.system(command)
-    ids = input("Enter the ids of the pages you want to delete separated by commas: ")
-    ids = ids.split(",")
-    for id in ids:
-        os.system(f"wp post delete {id} --force")
+    print(f'post_type: {post_type}')
+    multiple_or_all = input(
+        "Do you want to delete multiple posts or all? (m/a): ")
+    if multiple_or_all == "m":
+        command = f"wp post list --post_type={post_type}"
+        os.system(command)
+        ids = input(
+            "Enter the ids of the pages you want to delete separated by commas: ")
+        ids = ids.split(",")
+        for id in ids:
+            os.system(f"wp post delete {id} --force")
+    elif multiple_or_all == "a":
+        command = f"wp post list --post_type={post_type}"
+        os.system(command)
+        all_ids = os.popen(
+            f"wp post list --post_type={post_type} --field=ID").read().strip().split("\n")
+        for id in all_ids:
+            os.system(f"wp post delete {id} --force")
+    else:
+        print("Invalid option. Please choose 'm' or 'a'.")
+
 
 def listPages(post_type="page"):
     command = f"wp post list --post_type={post_type}"
     os.system(command)
 
+
 def duplicate(post_type="page"):
     command = f"wp post list --post_type={post_type}"
     os.system(command)
-    ids = input("Enter the id of the posts you want to duplicate, separated by comma: ")
+    ids = input(
+        "Enter the id of the posts you want to duplicate, separated by comma: ")
     ids = ids.split(",")
     for id in ids:
         command = f"wp post create --from-post={id}"
         os.system(command)
+
 
 def wpPages(post_type=False):
     if post_type:
@@ -56,8 +78,10 @@ def wpPages(post_type=False):
             post_type = post_type
     else:
         post_type = "page"
-    listPages()
-    def menu():
+
+    listPages(post_type)
+
+    def menu(post_type):
         print(colored("1) List pages", "green"))
         print(colored("1.1) Ignore content", "blue"))
         print(colored("1.2) Duplicate", "blue"))
@@ -70,37 +94,38 @@ def wpPages(post_type=False):
         choice = input("Make your choice:")
         if choice == "1":
             listPages(post_type)
-            menu()
+            menu(post_type)
         if choice == "1.1":
             ignore()
-            menu()
+            menu(post_type)
         if choice == "1.2":
             duplicate(post_type)
-            menu()
+            menu(post_type)
         if choice == "2":
             createOne(post_type)
             listPages(post_type)
-            menu()
+            menu(post_type)
         if choice == "3":
             createMultiple(post_type)
             listPages(post_type)
-            menu()
+            menu(post_type)
         elif choice == "4":
             deleteOne(post_type)
             listPages(post_type)
-            menu()
+            menu(post_type)
         elif choice == "5":
             deleteMultiple(post_type)
             listPages(post_type)
-            menu()
+            menu(post_type)
         else:
             exit(colored("Goodbye!", "red"))
 
-    menu()
+    menu(post_type)
+
 
 def ignore():
     listPages()
     user = os.getenv('USER')
-    path_to_wp_init = "/home/" + str(user) + "/Documents/python/wp-python/utils/info.sh"
+    path_to_wp_init = "/home/" + \
+        str(user) + "/Documents/python/wp-python/utils/info.sh"
     subprocess.run(["bash", path_to_wp_init])
-
