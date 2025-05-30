39979bd6fbb371a3c7d865c7a8118286b6a014e4
serii burduja <seriiburduja@gmail.com>
2025-05-30 08:23:42 +0300

    feat: pages

--- utils/pages.py
+++ utils/pages.py
@@ -3,10 +3,14 @@ import os
 from simple_term_menu import subprocess
 from termcolor import colored
 
+from classes.WpCli import WpCli
+
+wp_cli = WpCli()
+
 def createOne(post_type="page"):
     title = input("Enter the title: ")
-    command = f"wp post create --post_type={post_type} --post_status=publish --post_title='{title}'"
-    os.system(command)
+    command = f"post create --post_type={post_type} --post_status=publish --post_title='{title}'"
+    wp_cli.runWp(command)
 
 
 def createMultiple(post_type="page"):
@@ -15,60 +19,60 @@ def createMultiple(post_type="page"):
     titles = titles.split(",")
     for title in titles:
         title = title.strip()
-        command = f"wp post create --post_type={post_type} --post_status=publish --post_title='{title}'"
-        os.system(command)
+        command = f"post create --post_type={post_type} --post_status=publish --post_title='{title}'"
+        wp_cli.runWp(command)
 
 
 def deleteOne(post_type="page"):
-    command = f"wp post list --post_type={post_type}"
-    os.system(command)
+    command = f"post list --post_type={post_type}"
+    wp_cli.runWp(command)
     ids = input("Enter the id of the page you want to delete: ")
-    os.system(f"wp post delete {ids} --force")
+    command = f"post delete {ids} --force"
+    wp_cli.runWp(command)
 
 
 def deleteMultiple(post_type="page"):
     print(f'post_type: {post_type}')
-    multiple_or_all = input(
-        "Do you want to delete multiple posts or all? (m/a): ")
-    if multiple_or_all == "m":
-        command = f"wp post list --post_type={post_type}"
-        os.system(command)
-        ids = input(
-            "Enter the ids of the pages you want to delete separated by commas: ")
-        ids = ids.split(",")
-        for id in ids:
-            os.system(f"wp post delete {id} --force")
-    elif multiple_or_all == "a":
-        command = f"wp post list --post_type={post_type}"
-        os.system(command)
-        all_ids = os.popen(
-            f"wp post list --post_type={post_type} --field=ID").read().strip().split("\n")
-        for id in all_ids:
-            os.system(f"wp post delete {id} --force")
-    else:
-        print("Invalid option. Please choose 'm' or 'a'.")
+    # multiple_or_all = input(
+    #     "Do you want to delete multiple posts or all? (m/a): ")
+    # if multiple_or_all == "m":
+    #     command = f"post list --post_type={post_type}"
+    #     wp_cli.runWp(command)
+    #     ids = input(
+    #         "Enter the ids of the pages you want to delete separated by commas: ")
+    #     ids = ids.split(",")
+    #     for id in ids:
+    #         os.system(f"wp post delete {id} --force")
+    # elif multiple_or_all == "a":
+    #     command = f"post list --post_type={post_type}"
+    #     wp_cli.runWp(command)
+    #     all_ids = os.popen(f"post list --post_type={post_type} --field=ID").read().strip().split("\n")
+    #     for id in all_ids:
+    #         os.system(f"wp post delete {id} --force")
+    # else:
+    #     print("Invalid option. Please choose 'm' or 'a'.")
 
 
 def listPages(post_type="page"):
-    command = f"wp post list --post_type={post_type}"
-    os.system(command)
+    command = f"post list --post_type={post_type}"
+    wp_cli.runWp(command)
 
 
 def duplicate(post_type="page"):
-    command = f"wp post list --post_type={post_type}"
-    os.system(command)
+    command = f"post list --post_type={post_type}"
+    wp_cli.runWp(command)
     ids = input(
         "Enter the id of the posts you want to duplicate, separated by comma: ")
     ids = ids.split(",")
     for id in ids:
-        command = f"wp post create --from-post={id}"
-        os.system(command)
+        command = f"post create --from-post={id}"
+        wp_cli.runWp(command)
 
 
 def wpPages(post_type=False):
     if post_type:
-        command = "wp post-type list --capability_type=post --fields=name,public"
-        os.system(command)
+        command = "post-type list --capability_type=post --fields=name,public"
+        wp_cli.runWp(command)
         post_type = input("Enter the post type: ")
         if post_type == "":
             print("Post type is required")
