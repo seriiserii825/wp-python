a371b1c19ba9a7eacdaec53b7306152922201c45
serii burduja <seriiburduja@gmail.com>
2025-05-30 07:59:19 +0300

    feat: base plugins added all in one migration unlimited

--- data/base_plugins.py
+++ data/base_plugins.py
@@ -1,10 +1,11 @@
 base_plugins = [
-        {"advanced-custom-fields-pro": "advanced-custom-fields-pro-6_0_6.zip"},
-        {"all-in-one-wp-migration": "all-in-one-wp-migration-7-79.zip"},
-        {"classic-editor": False},
-        {"tinymce-advanced": False},
-        {"stops-core-theme-and-plugin-updates": False},
-        {"svg-support": False},
-        {"wps-hide-login": False},
-        {"wps-limit-login": False},
-        ]
+    {"advanced-custom-fields-pro": "advanced-custom-fields-pro-6_0_6.zip"},
+    {"all-in-one-wp-migration": "all-in-one-wp-migration-7-79.zip"},
+    {"all-in-one-wp-migration-unlimited": "unlimited/all-in-one-wp-migration-unlimited-extension-2.51.zip"},
+    {"classic-editor": False},
+    {"tinymce-advanced": False},
+    {"stops-core-theme-and-plugin-updates": False},
+    {"svg-support": False},
+    {"wps-hide-login": False},
+    {"wps-limit-login": False},
+]
