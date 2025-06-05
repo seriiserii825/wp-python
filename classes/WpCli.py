0a30c4b79265195af9c6144ac566bfe31a200166
serii burduja <seriiburduja@gmail.com>
2025-06-05 20:31:27 +0300

    feat: wp-cli

--- classes/WpCli.py
+++ classes/WpCli.py
@@ -1,24 +1,4 @@
 import os
-import subprocess
 class WpCli():
     def runWp(self, command: str):
         os.system(f"HOST_UID=$(id -u) HOST_GID=$(id -g) docker-compose run --rm wpcli {command}")
-        # command_arr = command.split(" ")
-        # full_cmd = ["docker-compose", "run", "--rm", "wpcli"] + command_arr
-        #
-        # print(f"Running command: {' '.join(full_cmd)}")
-        #
-        # process = subprocess.Popen(
-        #     full_cmd,
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.STDOUT,
-        #     universal_newlines=True,
-        #     bufsize=1
-        # )
-        #
-        # # Print output in real-time
-        # for line in process.stdout:
-        #     print(line, end="")
-        #
-        # process.wait()
-        # print(f"Exit code: {process.returncode}")
