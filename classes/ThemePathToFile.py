a98c8837f5fec0d5965f45d1edd7e0bae83cbcd3
serii burduja <seriiburduja@gmail.com>
2025-07-14 12:37:21 +0300

    feat: theme to file

--- classes/ThemePathToFile.py
+++ classes/ThemePathToFile.py
@@ -9,16 +9,15 @@ class ThemePathToFile:
         exit("[red]Please run this script from the root of your theme folder!")
 
     python_script_dir_path = Path(__file__).resolve().parent.parent
-    print(f"python_script_dir_path: {python_script_dir_path}")
+    # print(f"python_script_dir_path: {python_script_dir_path}")
     theme_name = path.parts[-1]
     theme_dir_abs_path = path.resolve()
-    print(f"theme_dir_abs_path: {theme_dir_abs_path}")
+    # print(f"theme_dir_abs_path: {theme_dir_abs_path}")
     theme_file_path = f"{python_script_dir_path}/wp_theme_path.txt"
-    print(f"theme_file_path: {theme_file_path}")
+    # print(f"theme_file_path: {theme_file_path}")
 
     @classmethod
     def theme_path_to_file(cls):
         with open(cls.theme_file_path, 'w') as file:
             file.write(str(cls.theme_dir_abs_path))
         print(f"[green]Theme path saved to {cls.theme_file_path}")
-
